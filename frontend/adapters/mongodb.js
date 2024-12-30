43ab44d89a5821c3e82c1b4b719f6ac994dc78e8
auspy <vinayak.kshetez0102@gmail.com>
2024-12-30 19:57:11 +0530

    refactor: update the mongodb adapter

--- frontend/adapters/mongodb.js
+++ frontend/adapters/mongodb.js
@@ -1,6 +1,5 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb";
-import { PINECONE_INDEX } from "@/constants";
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
@@ -25,33 +24,11 @@ if (process.env.NODE_ENV === "development") {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
-
-if (clientPromise) {
-  clientPromise.then((client) => {
-    client
-      .db(dbsForPineconeIndex[PINECONE_INDEX])
-      .collection("videos")
-      .createIndex(
-        { ytVidId: 1 },
-        {
-          unique: true,
-        },
-      );
-  });
-}
-
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
 export default clientPromise;
-const dbsForPineconeIndex = {
-  "100xdevs": "100xdevs_vizolv",
-  "video-semantic-search": "videoai",
-  "yt-vizolv": "yt_vizolv",
-};
-export async function getMongoClient(dbName, dbRequired = false) {
-  if (dbRequired && !dbName) {
-    throw new Error("Database name is required");
-  }
+
+export async function getMongoClient(dbName = "picture_outline") {
   const client = await clientPromise;
-  return client.db(dbName || dbsForPineconeIndex[PINECONE_INDEX]);
+  return client.db(dbName);
 }
