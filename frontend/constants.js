74014cafbfcafab4b5579c16066f5c21fd5499a8
auspy <vinayak.kshetez0102@gmail.com>
2024-12-31 01:16:12 +0530

    refactor: make python backend public

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,7 +1,7 @@
 export const urlBase =
   process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 export const urlPython =
-  process.env.PYTHON_BACKEND_URL || "http://localhost:8000";
+  process.env.NEXT_PUBLIC_PYTHON_BACKEND_URL || "http://localhost:8000";
 export const alt = "Picture Outline Generator";
 export const isDev = process.env.NODE_ENV === "development";
 export const MAX_FILE_SIZE =
